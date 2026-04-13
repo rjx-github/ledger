9be1c7893979f467957add86951024ed65712174
rjx-github <118156640+rjx-github@users.noreply.github.com>
2026-04-14 00:04:43 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ledger-v1';
+const FILES=['index.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(clients.claim())});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
