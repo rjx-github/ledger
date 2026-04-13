85c20e4757764b2492a3923c5dbcaabf9a6dc3d6
rjx-github <rjx-github@users.noreply.github.com>
2026-04-14 03:25:31 +0800

    fix: bump cache to v2, 柱状图居中

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ledger-v1';
+const CACHE='ledger-v2';
 const FILES=['index.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim())});
